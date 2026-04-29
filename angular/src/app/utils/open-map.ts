9c921725ea459c4044ee97bc7d1c88d033d8ac59
ferricx <chferrier@gmail.com>
2026-04-29 19:37:56 +0000

    Update address to 1847 County Rd 129, No Name, CO

--- angular/src/app/utils/open-map.ts
+++ angular/src/app/utils/open-map.ts
@@ -1,5 +1,5 @@
 export const MAPS_FALLBACK_URL =
-  'https://www.google.com/maps/search/?api=1&query=123+Any+St%2C+No+Name%2C+CO+81419';
+  'https://www.google.com/maps/search/?api=1&query=1847+County+Rd+129%2C+No+Name%2C+CO+81601';
 
 function tryScheme(uri: string): void {
   const a = document.createElement('a');
@@ -14,9 +14,9 @@ export function openMap(event: Event): void {
   event.preventDefault();
   const ua = navigator.userAgent;
   if (/Android|CrOS/.test(ua)) {
-    tryScheme('geo:0,0?q=123+Any+St%2C+No+Name%2C+CO+81419');
+    tryScheme('geo:0,0?q=1847+County+Rd+129%2C+No+Name%2C+CO+81601');
   } else if (/iPad|iPhone|iPod|Macintosh/.test(ua)) {
-    tryScheme('maps://?q=123+Any+St%2C+No+Name%2C+CO+81419');
+    tryScheme('maps://?q=1847+County+Rd+129%2C+No+Name%2C+CO+81601');
   } else {
     window.open(MAPS_FALLBACK_URL, '_blank', 'noopener');
   }
